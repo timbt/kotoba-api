936e888ccd21d844856b4ccf7e52825b0692e738
Tim Beattie <trbeattie@outlook.com>
2026-01-12 17:08:27 -0500

    Move initial unit tests into unit test subdirectory

--- tests/unit/test_resolvers.py
+++ tests/unit/test_resolvers.py
@@ -1,6 +1,6 @@
 from kotoba_api.app import resolve_hello
 
 
-def test_resolve_hello():
+def test_hello_resolver_returns_expected_string():
     result = resolve_hello(None, None)
     assert result == "Hello world!"
