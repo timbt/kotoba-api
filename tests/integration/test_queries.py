187e627325e0af32993c73e42b0bc88042da50ee
Tim Beattie <trbeattie@outlook.com>
2026-01-13 18:09:54 -0500

    Create integration test for kanji query

--- tests/integration/test_queries.py
+++ tests/integration/test_queries.py
@@ -15,3 +15,18 @@ def test_hello_query(client):
     payload = response.json()
     assert "errors" not in payload
     assert payload["data"]["hello"] == "Hello world!"
+
+
+def test_kanji_query(client):
+    response = client.post(
+        "/graphql/",
+        json={
+            "query": """
+                query {
+                    kanji(character: "猫")
+                }
+            """
+        },
+    )
+
+    assert response.status_code == 200
