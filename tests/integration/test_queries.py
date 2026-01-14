3e987e7b054855d6db1b22465852406381526f25
Tim Beattie <trbeattie@outlook.com>
2026-01-13 20:50:48 -0500

    Comment out test_kanji_query integration test for now

--- tests/integration/test_queries.py
+++ tests/integration/test_queries.py
@@ -16,20 +16,19 @@ def test_hello_query(client):
     assert "errors" not in payload
     assert payload["data"]["hello"] == "Hello world!"
 
-
-def test_kanji_query(client):
-    response = client.post(
-        "/graphql/",
-        json={
-            "query": """
-                query {
-                    kanji(literal: "猫") {
-                        literal
-                        meaning
-                    }
-                }
-            """
-        },
-    )
+    # def test_kanji_query(client):
+    #     response = client.post(
+    #         "/graphql/",
+    #         json={
+    #             "query": """
+    #                 query {
+    #                     kanji(literal: "猫") {
+    #                         literal
+    #                         meaning
+    #                     }
+    #                 }
+    #             """
+    #         },
+    #     )
 
     assert response.status_code == 200
