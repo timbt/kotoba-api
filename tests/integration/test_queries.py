3584568e10a7948cdaba18144e66556429485d7e
Tim Beattie <trbeattie@outlook.com>
2026-02-19 15:54:09 -0500

    Extend kanji query integration test to query for values on multiple kanji fields

--- tests/integration/test_queries.py
+++ tests/integration/test_queries.py
@@ -42,3 +42,6 @@ def test_kanji_query(client: TestClient):
     payload = response.json()
     assert "errors" not in payload
     assert payload["data"]["kanji"]["literal"] == "猫"
+    assert "ビョウ" in payload["data"]["kanji"]["readings_on"]
+    assert "ねこ" in payload["data"]["kanji"]["readings_kun"]
+    assert "cat" in payload["data"]["kanji"]["meanings"]
