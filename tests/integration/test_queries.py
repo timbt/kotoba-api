c7a2f0a696afdb3dad531837ae1e8456b2436bff
Tim Beattie <trbeattie@outlook.com>
2026-04-02 17:34:53 -0400

    Create first integration test for search query

--- tests/integration/test_queries.py
+++ tests/integration/test_queries.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 
@@ -87,3 +88,32 @@ def test_kanji_search_by_meaning(client: TestClient):
     assert "ビョウ" in neko["readings_on"]
     assert "ねこ" in neko["readings_kun"]
     assert "cat" in neko["meanings"]
+
+
+@pytest.mark.xfail(reason="Feature not yet implemented", strict=True)
+def test_search_query_search_by_literal(client: TestClient):
+    response = client.post(
+        "/graphql/",
+        json={
+            "query": """"
+                query {
+                    search(searchQuery: "猫") {
+                        searchQuery
+                        kanji {
+                            literal
+                            readings_on
+                            readings_kun
+                            meanings
+                        }
+                    }
+                }
+            """
+        },
+    )
+
+    assert response.status_code == 200
+
+    payload = response.json()
+
+    # Verify correct query was used in search
+    assert payload["data"]["meaning"] == "猫"
